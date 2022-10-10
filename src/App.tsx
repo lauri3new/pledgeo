05859772fe4015daa2bb6514ae598ebad7b801d0
laurence newman <laurence@poweredbypercent.com>
2022-10-11 00:03:10 +0100

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardImg,
+  CardBody,
+  CardFooter,
+  Button,
+  Container,
+  Row,
+  Col
+} from "shards-react";
+import axios from "axios"
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
+
+const client = axios.create({
+  baseURL: 'https://us-central1-pledgeo.cloudfunctions.net/api/'
+})
 
 function App() {
+  const [loading, setLoading] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row style={{ textAlign: 'center', padding: 50 }}>
+        <Col>
+        <h1 style={{ fontSize: 80, fontWeight: 'bold'}}>Pledgeo 🌍</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={4}>
+        <Card>
+      <CardHeader>Card header</CardHeader>
+      <CardImg src="https://wbg.org.uk/wp-content/uploads/2020/04/Oxfam-Logo.jpg" />
+      <CardBody>
+        <CardTitle>Oxfam</CardTitle>
+        <p>To prevent and relieve poverty and to protect the vulnerable, including through humanitarian intervention; . To advance sustainable development; To promote human rights and equality and diversity, in particular where to do so contributes to the prevention and relief of poverty;</p>
+        <Button
+          disabled={loading}
+          onClick={() => {
+            setLoading(true)
+            client.post('donation-sessions', {
+              organisationId: "staging_organisation_000000C6pZ3jKMdNUr0hMtgoYuDnf"
+            })
+            .then(({ data }) => {
+              window.location = data.data.url
+            })
+            .catch(e => {
+              console.log(e)
+            })
+          }}
+        >Donate</Button>
+      </CardBody>
+      <CardFooter>
+        <p>GBR</p>
+        <p>2700 John Smith Drive</p>
+      </CardFooter>
+    </Card>
+        </Col>
+      </Row>
+      <Row style={{ height: 200 }}>
+        <Col>
+          <hr style={{ marginTop: 80, paddingBottom: 20 }}/>
+          <p>Pledgeo&nbsp;&nbsp;&nbsp;<strong>2022</strong></p>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
