c94177a6eeac274bca6cf62669b0aff0819f6d84
laurence newman <laurence@poweredbypercent.com>
2022-10-11 09:24:47 +0100

    ^^

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
     <Container>
       <Row style={{ textAlign: 'center', padding: 50 }}>
         <Col>
-        <h1 style={{ fontSize: 80, fontWeight: 'bold', lineHeight: 1.2}}>Pledgeo 🌍</h1>
+        <h1 style={{ fontSize: 70, fontWeight: 'bold', lineHeight: 1.2}}>Pledgeo 🌍</h1>
         </Col>
       </Row>
       <Row>
